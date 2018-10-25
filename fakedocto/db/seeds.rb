1717ab4fe4f8c40dd5e2b10d6b1a30874b04c654
Camille Duquenne <neeira@live.fr>
2018-10-25 15:46:29 +0200

    doctolol

---
+++ fakedocto/db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+    doctor = Doctor.create!(first_name: Faker::Name.female_first_name, last_name: Faker::Name.last_name, specialty: Faker::Hobbit.character, postal_code:Faker::Address.zip_code)
+end
+
+10.times do
+    patient = Patient.create!(first_name: Faker::Name.male_first_name, last_name: Faker::Name.last_name)
+end
+
+10.times do
+    appointment = Appointment.create!(date: Faker::Date.forward(230), doctor_id: Faker::Number.between(1, 10), patient_id: Faker::Number.between(1, 10))
+end
+
